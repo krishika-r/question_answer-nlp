16c42801db7fb0cbe6ead2779d1612ce10004fc5
krishika-r <36788439+krishika-r@users.noreply.github.com>
2023-05-11 19:29:37 +0530

    added docstrings

--- helper.py
+++ helper.py
@@ -9,73 +9,93 @@ import numpy as np
 import pandas as pd
 import pathlib
 
-def generate_jsonl(data, prediction_path, model_name, data_type):
+def generate_jsonl(data : str, prediction_path : str, model_name : str, data_type : str):
+  """
+  Generate jsonl file for the model prediction json file
 
-    # input file type
-    file_extension=pathlib.Path(data).suffix
-    if file_extension =='.csv':
-        # reading csv file
-          raw_df = pd.read_csv(data)
+  Parameters
+  ----------
+    data (str) : train/validate/test data path
+    prediction_path (str) : Model pediction output path
+    model_name (str) : Huggingface qna model names
+    data_type (str) : train/validation/test 
 
-    if file_extension =='.json':
-          # reading json file
-          raw_df = open(data)
-          raw_df = json.load(raw_df)
-          raw_df=json_normalize(raw_df['data'])
+  Returns
+  ----------
+    None
+  """
+  # input file type
+  file_extension=pathlib.Path(data).suffix
+  if file_extension =='.csv':
+      # reading csv file
+        raw_df = pd.read_csv(data)
 
-    if 'answers.text' in raw_df.columns:
-        raw_df=raw_df[["context","question","answers.text"]]    
-        raw_df['answer'] = raw_df['answers.text'].apply(lambda x:" ".join(x))
-        raw_df = raw_df.drop(columns = ['answers.text'])
-    else:
-        raw_df=raw_df[["context","question"]] 
-        
-    # reading predicted file
-    if data_type == "train" or data_type == "test":
-      f= open(os.path.join(prediction_path, "predict_predictions.json"))
-      data = json.load(f)
-      predicted_answer=[]
-      for x in data:
-        predicted_answer.append(data[x])
-    else:
-      f = open(os.path.join(prediction_path, "eval_predictions.json"))
-      data = json.load(f)
-      predicted_answer=[]
-      for x in data:
-        predicted_answer.append(data[x])
-        
-    # creating dataframe
-    final_df = raw_df.copy()
-    final_df['predicted_answer'] = predicted_answer
-    final_df['model_name'] = model_name
-    final_df['data_type'] = data_type
-    final_df.to_json(
-      os.path.join(prediction_path, f"{data_type}_data_predictions_processed.jsonl"),orient="records",)
+  if file_extension =='.json':
+        # reading json file
+        raw_df = open(data)
+        raw_df = json.load(raw_df)
+        raw_df=json_normalize(raw_df['data'])
 
-    if 'answer' in final_df.columns:
-    # Benchmarking models
-      bleu_score, rouge1, rougeL, semantic_similarity, _df_ = get_evaluation_metrics(final_df['answer'].tolist(), final_df['predicted_answer'].tolist())
-      final_df['bleu_score'] = _df_['bleu_score'].tolist()
-      final_df['rouge1'] = _df_['rouge1'].tolist()
-      final_df['rougeL'] = _df_['rougeL'].tolist()
-      final_df['sentence_similarity'] = _df_['sentence_similarity'].tolist()
-
-      benchmark_df = pd.DataFrame({'model_name':[model_name],'type':[data_type], 'bleu_score':[bleu_score], 'rouge1':[rouge1], 
-                                  'rougeL':[rougeL], 'semantic_similarity':[semantic_similarity]})
+  if 'answers.text' in raw_df.columns:
+      raw_df=raw_df[["context","question","answers.text"]]    
+      raw_df['answer'] = raw_df['answers.text'].apply(lambda x:" ".join(x))
+      raw_df = raw_df.drop(columns = ['answers.text'])
+  else:
+      raw_df=raw_df[["context","question"]] 
       
-      benchmark_df.to_csv(os.path.join(prediction_path, f"{data_type}_data_benchmarks.csv"),index=False)
+  # reading predicted file
+  if data_type == "train" or data_type == "test":
+    f= open(os.path.join(prediction_path, "predict_predictions.json"))
+    data = json.load(f)
+    predicted_answer=[]
+    for x in data:
+      predicted_answer.append(data[x])
+  else:
+    f = open(os.path.join(prediction_path, "eval_predictions.json"))
+    data = json.load(f)
+    predicted_answer=[]
+    for x in data:
+      predicted_answer.append(data[x])
+      
+  # creating dataframe
+  final_df = raw_df.copy()
+  final_df['predicted_answer'] = predicted_answer
+  final_df['model_name'] = model_name
+  final_df['data_type'] = data_type
+  # saving final dataframe to jsonl format
+  final_df.to_json(
+    os.path.join(prediction_path, f"{data_type}_data_predictions_processed.jsonl"),orient="records",)
+
+  if 'answer' in final_df.columns:
+    # Benchmarking models
+    bleu_score, rouge1, rougeL, semantic_similarity, _df_ = get_evaluation_metrics(final_df['answer'].tolist(), final_df['predicted_answer'].tolist())
+    final_df['bleu_score'] = _df_['bleu_score'].tolist()
+    final_df['rouge1'] = _df_['rouge1'].tolist()
+    final_df['rougeL'] = _df_['rougeL'].tolist()
+    final_df['sentence_similarity'] = _df_['sentence_similarity'].tolist()
+
+    benchmark_df = pd.DataFrame({'model_name':[model_name],'type':[data_type], 'bleu_score':[bleu_score], 'rouge1':[rouge1], 
+                                'rougeL':[rougeL], 'semantic_similarity':[semantic_similarity]})
+
+    #saving benchmark dataframe to csv
+    benchmark_df.to_csv(os.path.join(prediction_path, f"{data_type}_data_benchmarks.csv"),index=False)
 
 
-def get_sentence_similarity(reference = '', generated = '',  model_name = None):
-  '''
+def get_sentence_similarity(reference :str = '', generated : str= '',  model_name :str = None):
+  """
   Generate cosine similarity score based on embeddings of two strings
-  Parameters:
+
+  Parameters
+  ----------
     reference (str) : Reference string to check similarity
     generated (str) : Generated/Target string to check similarity
     model_name (str) : Sentence tranformer model names
-  Returns:
+        If set as `None`, default model : "all-minilm-l6-v2" is considered
+
+  Returns
+  ----------
     Similarity score (float) : Cosine similarity score based on embeddings of the two strings
-  '''
+  """
   if model_name == None:
     model = SentenceTransformer('all-minilm-l6-v2')
   else:
@@ -90,35 +110,40 @@ def get_sentence_similarity(reference = '', generated = '',  model_name = None):
 
   return cosine_scores.item() 
 
-def get_bleu_score(reference, candidate):
-  '''
+def get_bleu_score(reference :str, candidate :str):
+  """
   Function to get BLEU scores for two strings
 
-  Parameters:
+  Parameters
+  ----------
     reference (str) : Reference String
     candidate (str) : Candidate String
 
-  Returns: 
+  Returns
+  ----------
     (float) : BLEU score
-  '''
+  """
   candidate_ = candidate.split() 
   reference_ = []
   reference_.append(reference.split())
   return sentence_bleu(reference_, candidate_, weights=(1, 0, 0, 0))
 
-def get_evaluation_metrics(actuals, predicted):
-  '''
+def get_evaluation_metrics(actuals :str, predicted :str):
+  """
   Generate benchamrking scores on different metrics for generated text
 
-  Parameters:
+  Parameters
+  ----------
     actuals (str | list) : Actual text or reference
     predicted (str | list) : Generated text or predictions
-  Returns:
+
+  Returns
+  ----------
     blue_score (float) : Mean BLUE score
     rouge1 (float): Mean ROUGE1 score
     rougeL (float): Mean ROUGEL score
     sentence similarity (float): Mean Cosine Similariy score on embeddedings
-  '''
+  """
   if isinstance(actuals, list) and isinstance(predicted, list):
     df = pd.DataFrame({'actuals':actuals,'predicted':predicted})
   elif isinstance(actuals, str) and isinstance(predicted, str):
--- question_answer.py
+++ question_answer.py
@@ -1,4 +1,4 @@
-"""The script for training/inferencing a question answering
+"""The script for training and inferencing a question answering model
 """
 
 import pandas as pd
@@ -14,6 +14,14 @@ from helper import generate_jsonl
 import pathlib
 
 class QnA:
+    """ QnA is an extractive question answering technique that extracts answer
+        from the context. This is usually solved using BERT-like models.
+        
+    Examples
+    --------
+    >>> from question_answer import QnA
+    >>> qna=QnA()
+    """
     def __init__(self):
         self.max_answer_length = 50
         self.per_device_train_batch_size = 8 
@@ -22,8 +30,48 @@ class QnA:
         self.max_seq_length = 384 
         self.doc_stride = 128 
         self.n_best_size = 20
+
+        """QnA initialization
+
+        Parameters
+        ----------
+        max_answer_length: The maximum length of an answer that can be generated. This is needed because the start 
+                           and end predictions are not conditioned on one another.
+        per_device_train_batch_size: The batch size per GPU/TPU core/CPU for training
+        learning_rate: The initial learning rate for ADAM
+        num_train_epochs: Total number of training epochs to perform.
+        max_seq_length: The maximum total input sequence length after tokenization. Sequences longer than this will be truncated,
+                        sequences shorter will be padded.
+        doc_stride: When splitting up a long document into chunks, how much stride to take between chunks
+        n_best_size: The total number of n-best predictions to generate when looking for an answer
+        """
         
     def train(self, train_data_path : str, output_path : str, model_name: Optional[str]=None, valn_path :Optional[str]=None, **kwargs):
+        """Function used to fine tune any huggingface qna models.
+
+        Parameters
+        ----------
+        train_data_path: str
+            Training data file/path of csv or json file
+        output_path: str
+            Output directory to store the finetuned model
+        model_name: None, optional (str)
+            If set as `None`, default model : "deepset/tinyroberta-squad2" is considered
+        valn_path : None, optional (str)
+            An optional validation data file/path to evaluate the perplexity on (a csv or json file)
+        kwargs: default parameters
+            Any default parameters can be used for fine tuning the model. eg:learning_rate,doc_stride etc..
+
+        Returns
+        -------
+            None
+
+        Examples
+        --------
+        >>> from question_answer import QnA
+        >>> qna=QnA()
+        >>> finetuned_model=qna.train(train_data_path='train.json/csv',output_path='output_directory',model_name='deepset/roberta-base-squad2',valn_path='validate.json/csv')
+        """
         self.train_data_path = train_data_path
         self.valn_path = valn_path
         self.output_path = output_path
@@ -99,11 +147,12 @@ class QnA:
                     check=True,
                     capture_output=True,
                 )
+                print("Trainer output: ",trainer.stdout)
             except CalledProcessError as err:
                 print("error message",err)
                 print(err.stderr.decode('utf8'))
 
-            # saving jsonl output for validation file
+            # saving predicted output as jsonl for validation file
             generate_jsonl(
                 self.valn_path,
                 self.output_path,
@@ -122,11 +171,12 @@ class QnA:
                     check=True,
                     capture_output=True,
                 )
+                print("Trainer output: ",trainer.stdout)
+
             except CalledProcessError as err:
                 print("error message",err)
                 print(err.stderr.decode('utf8'))
 
-        print("Trainer output: ",trainer.stdout)
 
         if self.train_data_path:
             print("train prediction")
@@ -150,23 +200,46 @@ class QnA:
 
     def predict(
         self,
-        question : str=None,
-        context : str=None,
-        model_name : Optional[str]=None,
-        output_path : str =None,
+        question : str = None,
+        context : str = None,
+        model_name : Optional[str] = None,
+        output_path : str = None,
         test_path: str = None,
         is_train=False, 
         **kwargs):
-        """
-        Inference function
 
-        Parameters:
-        model_name_or_path (str):model name (extractive qna)
-        question,context (str) or dataframe
+        """Inference Function to test an input/file.
 
-        Returns:
-        dict: predicted answer
-        
+        Parameters
+        ----------
+        question: str
+            An input string
+        context: str
+            An input string
+        test_path: str
+            Test data file/path to evaluate the perplexity on (a csv or json file)
+        output_path: str
+            Output directory to store the test file predicted results
+        model_name: None, optional (str)
+            If set as `None`, default model : "deepset/tinyroberta-squad2" is considered
+        is_train: False
+            If is_train=False(Default) used to save predicted test result in jsonl format or if is_train=True used to save predicted train result in jsonl format.
+        kwargs: default parameters
+            Any default parameters can be used for prediction. eg:learning_rate,doc_stride etc..
+
+        Returns
+        -------
+        dict | output directory
+            Dict containing question,context,predicted_answer,model_name,score,answer_start,answer_end values.
+            Predicted test file are stored in output directory.
+
+        Examples
+        --------
+        >>> from question_answer import QnA
+        >>> qna=QnA()
+        >>> qna.predict(test_path="test.json/csv",output_path="output_dir",model_name='deepset/roberta-base-squad2')
+        >>> # or
+        >>> qna.predict(question=question,context=context,doc_stride=128,max_answer_length=20,learning_rate=3e-5,n_best_size=20)
         """
         if not model_name:
             model_name = "deepset/tinyroberta-squad2"
@@ -175,7 +248,13 @@ class QnA:
         print("Executed")
         print('Model Name: {}'.format(model_name))
 
-        if isinstance(context,str) and isinstance(question,str):        
+        # Predicting answers when context and question are string dtype
+        if isinstance(context,str) or isinstance(question,str): 
+            if not context or not question:
+                raise NameError(
+                    "Please enter the context/question as a string"
+                )  
+            #Infer QA model with transformers library using question-answering pipeline      
             question_answerer = pipeline('question-answering', model=model_name)
             res = question_answerer(question=question,context=context,**kwargs)
             df = pd.DataFrame([res])
@@ -184,13 +263,15 @@ class QnA:
             df['model_name'] = model_name
             df.rename(columns = {'answer':'predicted_answer'}, inplace = True)
             df= df[['question','context','predicted_answer','model_name','score','start','end']]
-            # dict
+            # Create dictionary
             dictionary = df.to_dict(orient="records")
+            # return dictionary
             return dictionary
 
+        #Predicting answers when context and question are specified in csv/json file
         else:
             if not test_path or not output_path:
-                raise TypeError(
+                raise NameError(
                     "Please enter the test path/output path"
                 )
             self.test_path = test_path
@@ -227,9 +308,9 @@ class QnA:
                 f"--max_seq_length={self.max_seq_length}",
                 f"--doc_stride={self.doc_stride}",
                 f"--n_best_size={self.n_best_size}",
-                "--overwrite_output_dir",  # to overwrite the existing files
                 "--output_dir",
                 self.output_path,
+                "--overwrite_output_dir",  # to overwrite the existing files
             ]
 
             # Start the training
@@ -240,15 +321,16 @@ class QnA:
                     check=True,
                     capture_output=True,
                 )
+                print("Evaluation output: ",evaluator.stdout)
+
             except CalledProcessError as err:
                 print("error message",err)
                 print(err.stderr.decode('utf8'))
-
+            
+            # saving predicted output as jsonl for test file
             generate_jsonl(
                 self.test_path,
                 self.output_path,
                 self.model_name,
                 "train" if is_train else "test",
             )
-
-            print("Evaluation output: ",evaluator.stdout)
